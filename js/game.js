5674c544ef21b9758c1e748553bfc0ce611c026c
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-05-06 11:51:24 +0100

    continued writing game setup

--- js/game.js
+++ js/game.js
@@ -1,12 +1,32 @@
-import {hello} from './modules/gameClasses.js';
-const canvas = document.getElementById('snake');
+import {SnakePiece} from './modules/gameClasses.js';
 
-document.onload = setup();
+const resolution = 10;
+const snakeCont = {
+  canvas : document.getElementById('snake'),
+  snakeArray : [],
+  setup : function() {
+    this.canvas.height = window.innerHeight / 2;
+    this.canvas.width = window.innerHeight / 2;
 
-function setup() {
-  canvas.height = window.innerHeight / 2;
-  canvas.width = window.innerHeight / 2;
+    this.ctx = this.canvas.getContext('2d');
 
-  console.log(canvas.width);
-  console.log(canvas.height);
+    let dim = 1 * resolution,
+    posX = this.canvas.width / 2 - (resolution / 2),
+    posY = this.canvas.height / 2 - (resolution / 2);
+    this.snakeArray.push(new SnakePiece(dim, dim, posX, posY, 0, 0, true, null));
+
+    this.ctx.font = '30px Arial';
+    this.ctx.fillStyle = 'black';
+    this.ctx.textAlign = 'center';
+    this.ctx.fillText('Press Space To Start', this.canvas.width / 2, this.canvas.height / 2);
+  },
+  logic : function() {
+
+  }
+}
+
+document.onload = snakeCont.setup();
+
+function gameStart() {
+
 }
