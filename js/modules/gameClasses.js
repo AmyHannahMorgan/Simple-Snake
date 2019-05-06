2f7fb3c3d67accc312e2f5549dadded42265ddc1
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-05-06 11:50:55 +0100

    created SnakePiece and Apple classes for game

--- js/modules/gameClasses.js
+++ js/modules/gameClasses.js
@@ -1,5 +1,32 @@
 export class SnakePiece {
-  constructor(head, tail, width, height, posX, posY, movX, movY) {
+  constructor(width, height, posX, posY, movX, movY, head, neighbor) {
+    this.width = width;
+    this.height = height;
+    this.pos = {
+      x : posX,
+      y : posY
+    };
+    this.movX = movX;
+    this.movY = movY;
+    this.head = head;
+    this.neighbor = neighbor;
+  }
 
+  getNeighborMov() {
+    if(!this.head) {
+
+    }
+  }
+}
+
+export class Apple {
+  constructor(width, height, posX, posY, score) {
+    this.width = width;
+    this.height = height;
+    this.pos = {
+      x : posX,
+      y : posY
+    };
+    this.score = score;
   }
 }
