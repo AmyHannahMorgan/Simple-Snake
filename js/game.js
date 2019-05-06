9f84f334855aeebb899f183450c3cf75ac9376c5
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2019-05-06 10:16:33 +0100

    created game.js and wrote setup function to set canvas size

---
+++ js/game.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('snake');
+
+document.onload = setup();
+
+function setup() {
+  canvas.height = window.innerHeight / 2;
+  canvas.width = window.innerHeight / 2;
+
+  console.log(canvas.width);
+  console.log(canvas.height);
+}
